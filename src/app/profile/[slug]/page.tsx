4ea4e91e3fabb3e20e02576f96df731d0a07b350
Shafiul Alam Faysal <57627778+Shafiul-Alam-Faysal@users.noreply.github.com>
2025-08-15 16:24:06 +0600

    implement rewrite ProfileSlugPage

--- src/app/profile/[slug]/page.tsx
+++ src/app/profile/[slug]/page.tsx
@@ -1,12 +1,31 @@
-export default function ProfileSlugPage({
-	// removed async
+// export default function ProfileSlugPage({
+// 	// removed async
+// 	params,
+// }: {
+// 	params: { slug: string };
+// }) {
+// 	// await params (even if it's just destructuring)
+// 	// const { slug } = await params;
+// 	const { slug } = params;
+
+// 	return (
+// 		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+// 			<h1>Profile</h1>
+// 			<p className="text-4xl">
+// 				Profile page <span className="p-2 bg-amber-700">{slug}</span>
+// 			</p>
+// 		</div>
+// 	);
+// }
+
+// src/app/profile/[slug]/page.tsx
+
+export default async function ProfileSlugPage({
 	params,
 }: {
-	params: { slug: string };
+	params: Promise<{ slug: string }>;
 }) {
-	// await params (even if it's just destructuring)
-	// const { slug } = await params;
-	const { slug } = params;
+	const { slug } = await params;
 
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen py-2">
